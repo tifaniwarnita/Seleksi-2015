e57ddfa0aa71c9fdebc2a17ad8bb2fd9025753fb
tifaniwarnita <tiffayumuyuka@gmail.com>
2015-06-26 00:42:00 +0700

    Dipercantik biar ga acak-acakan

--- assets/js/data/overview-ipm.js
+++ assets/js/data/overview-ipm.js
@@ -1,1 +1,131 @@
-$(function () {overviewMap = [{"kode_provinsi":"id-ac","nama_provinsi":"Prov. Nanggroe Aceh Darussalam","value\r":"71.7\r"},{"kode_provinsi":"id-su","nama_provinsi":"Prov. Sumatera Utara","value\r":"74.19\r"},{"kode_provinsi":"id-sb","nama_provinsi":"Prov. Sumatera Barat","value\r":"73.78\r"},{"kode_provinsi":"id-ri","nama_provinsi":"Prov. Riau","value\r":"76.07\r"},{"kode_provinsi":"id-ja","nama_provinsi":"Prov. Jambi","value\r":"72.74\r"},{"kode_provinsi":"id-sl","nama_provinsi":"Prov. Sumatera Selatan","value\r":"72.95\r"},{"kode_provinsi":"id-be","nama_provinsi":"Prov. Bengkulu","value\r":"72.92\r"},{"kode_provinsi":"id-1024","nama_provinsi":"Prov. Lampung","value\r":"71.42\r"},{"kode_provinsi":"id-bb","nama_provinsi":"Prov. Kepulauan Bangka Belitung","value\r":"72.86\r"},{"kode_provinsi":"id-kr","nama_provinsi":"Prov. Kepulauan Riau","value\r":"75.07\r"},{"kode_provinsi":"id-jk","nama_provinsi":"Prov. DKI Jakarta","value\r":"77.6\r"},{"kode_provinsi":"id-jr","nama_provinsi":"Prov. Jawa Barat","value\r":"72.29\r"},{"kode_provinsi":"id-jt","nama_provinsi":"Prov. Jawa Tengah","value\r":"72.49\r"},{"kode_provinsi":"id-yo","nama_provinsi":"Prov. D I Yogyakarta","value\r":"75.77\r"},{"kode_provinsi":"id-ji","nama_provinsi":"Prov. Jawa Timur","value\r":"71.62\r"},{"kode_provinsi":"id-bt","nama_provinsi":"Prov. Banten","value\r":"70.48\r"},{"kode_provinsi":"id-ba","nama_provinsi":"Prov. Bali","value\r":"72.28\r"},{"kode_provinsi":"id-nb","nama_provinsi":"Prov. Nusa Tenggara Barat","value\r":"65.2\r"},{"kode_provinsi":"id-nt","nama_provinsi":"Prov. Nusa Tenggara Timur","value\r":"67.26\r"},{"kode_provinsi":"id-kb","nama_provinsi":"Prov. Kalimantan Barat","value\r":"69.15\r"},{"kode_provinsi":"id-kt","nama_provinsi":"Prov. Kalimantan Tengah","value\r":"74.64\r"},{"kode_provinsi":"id-ks","nama_provinsi":"Prov. Kalimantan Selatan","value\r":"69.92\r"},{"kode_provinsi":"id-ki","nama_provinsi":"Prov. Kalimantan Timur","value\r":"75.56\r"},{"kode_provinsi":"id-sw","nama_provinsi":"Prov. Sulawesi Utara","value\r":"76.09\r"},{"kode_provinsi":"id-st","nama_provinsi":"Prov. Sulawesi Tengah","value\r":"71.14\r"},{"kode_provinsi":"id-se","nama_provinsi":"Prov. Sulawesi Selatan","value\r":"71.62\r"},{"kode_provinsi":"id-sg","nama_provinsi":"Prov. Sulawesi Tenggara","value\r":"70\r"},{"kode_provinsi":"id-go","nama_provinsi":"Prov. Gorontalo","value\r":"70.28\r"},{"kode_provinsi":"id-sr","nama_provinsi":"Prov. Sulawesi Barat","value\r":"69.64\r"},{"kode_provinsi":"id-ma","nama_provinsi":"Prov. Maluku","value\r":"71.42\r"},{"kode_provinsi":"id-la","nama_provinsi":"Prov. Maluku Utara","value\r":"69.03\r"},{"kode_provinsi":"id-pa","nama_provinsi":"Prov. Papua","value\r":"64.94\r"},{"kode_provinsi":""}];});
+$(function () {
+	overviewMap = [
+		{	"kode_provinsi":"id-ac",
+			"nama_provinsi":"Prov. Nanggroe Aceh Darussalam",
+			"value":71.7
+		},
+		{	"kode_provinsi":"id-su",
+			"nama_provinsi":"Prov. Sumatera Utara",
+			"value":74.19
+		},
+		{	"kode_provinsi":"id-sb",
+			"nama_provinsi":"Prov. Sumatera Barat",
+			"value":73.78
+		},
+		{	"kode_provinsi":"id-ri",
+			"nama_provinsi":"Prov. Riau",
+			"value":76.07
+		},
+		{	"kode_provinsi":"id-ja",
+			"nama_provinsi":"Prov. Jambi",
+			"value":72.74
+		},
+		{	"kode_provinsi":"id-sl",
+			"nama_provinsi":"Prov. Sumatera Selatan",
+			"value":72.95
+		},
+		{	"kode_provinsi":"id-be",
+			"nama_provinsi":"Prov. Bengkulu",
+			"value":72.92
+		},
+		{	"kode_provinsi":"id-1024",
+			"nama_provinsi":"Prov. Lampung",
+			"value":71.42
+		},
+		{	"kode_provinsi":"id-bb",
+			"nama_provinsi":"Prov. Kepulauan Bangka Belitung",
+			"value":72.86
+		},
+		{	"kode_provinsi":"id-kr",
+			"nama_provinsi":"Prov. Kepulauan Riau",
+			"value":75.07
+		},
+		{	"kode_provinsi":"id-jk",
+			"nama_provinsi":"Prov. DKI Jakarta",
+			"value":77.6
+		},
+		{	"kode_provinsi":"id-jr",
+			"nama_provinsi":"Prov. Jawa Barat",
+			"value":72.29
+		},
+		{	"kode_provinsi":"id-jt",
+			"nama_provinsi":"Prov. Jawa Tengah",
+			"value":72.49
+		},
+		{	"kode_provinsi":"id-yo",
+			"nama_provinsi":"Prov. D I Yogyakarta",
+			"value":75.77
+		},
+		{	"kode_provinsi":"id-ji",
+			"nama_provinsi":"Prov. Jawa Timur",
+			"value":71.62
+		},
+		{	"kode_provinsi":"id-bt",
+			"nama_provinsi":"Prov. Banten",
+			"value":70.48
+		},
+		{	"kode_provinsi":"id-ba",
+			"nama_provinsi":"Prov. Bali",
+			"value":72.28
+		},
+		{	"kode_provinsi":"id-nb",
+			"nama_provinsi":"Prov. Nusa Tenggara Barat",
+			"value":65.2
+		},
+		{	"kode_provinsi":"id-nt",
+			"nama_provinsi":"Prov. Nusa Tenggara Timur",
+			"value":67.26
+		},
+		{	"kode_provinsi":"id-kb",
+			"nama_provinsi":"Prov. Kalimantan Barat",
+			"value":69.15
+		},
+		{	"kode_provinsi":"id-kt",
+			"nama_provinsi":"Prov. Kalimantan Tengah",
+			"value":74.64
+		},
+		{	"kode_provinsi":"id-ks",
+			"nama_provinsi":"Prov. Kalimantan Selatan",
+			"value":69.92
+		},
+		{	"kode_provinsi":"id-ki",
+			"nama_provinsi":"Prov. Kalimantan Timur",
+			"value":75.56
+		},
+		{	"kode_provinsi":"id-sw",
+			"nama_provinsi":"Prov. Sulawesi Utara",
+			"value":76.09
+		},
+		{	"kode_provinsi":"id-st",
+			"nama_provinsi":"Prov. Sulawesi Tengah",
+			"value":71.14
+		},
+		{	"kode_provinsi":"id-se",
+			"nama_provinsi":"Prov. Sulawesi Selatan",
+			"value":71.62
+		},
+		{	"kode_provinsi":"id-sg",
+			"nama_provinsi":"Prov. Sulawesi Tenggara",
+			"value":70
+		},
+		{	"kode_provinsi":"id-go",
+			"nama_provinsi":"Prov. Gorontalo",
+			"value":70.28
+		},
+		{	"kode_provinsi":"id-sr",
+			"nama_provinsi":"Prov. Sulawesi Barat",
+			"value":69.64
+		},
+		{	"kode_provinsi":"id-ma",
+			"nama_provinsi":"Prov. Maluku",
+			"value":71.42
+		},
+		{	"kode_provinsi":"id-la",
+			"nama_provinsi":"Prov. Maluku Utara",
+			"value":69.03
+		},
+		{	"kode_provinsi":"id-pa",
+			"nama_provinsi":"Prov. Papua",
+			"value":64.94
+		}];
+});
